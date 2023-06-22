e945eaa4c79466a96d3741a778918f994dafcd7f
LucasGabrielAraujo <122766033+LucasGabrielAraujo@users.noreply.github.com>
2023-06-21 22:05:32 -0300

    deleted

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-var _config = require("./config");
-require("./database");
-_app["default"].listen(_config.PORT);
-console.log("Server en puerto", _config.PORT);
